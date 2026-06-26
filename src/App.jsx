f81d1927e70f29bc57bce1bd1c9712802bb0516b
Vinay Yadav <ydavvinay@gmail.com>
2026-06-27 01:29:38 +0530

    Added Suspense and Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./store/CartContext";
-import Home from "./components/Home";
-import Products from "./components/Products";
-import ProductDetail from "./components/ProductDetail";
-import About from "./components/About";
-import ContactUs from "./components/ContactUs";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import ChangePassword from "./components/ChangePassword"; // ✅ Import
+import { Container, Spinner } from "react-bootstrap";
 import Navigation from "./components/Navigation";
-import { Container } from "react-bootstrap";
+
+// ✅ Lazy Loading - Components on-demand load honge
+const Home = lazy(() => import("./components/Home"));
+const Products = lazy(() => import("./components/Products"));
+const ProductDetail = lazy(() => import("./components/ProductDetail"));
+const About = lazy(() => import("./components/About"));
+const ContactUs = lazy(() => import("./components/ContactUs"));
+const Signup = lazy(() => import("./components/Signup"));
+const Login = lazy(() => import("./components/Login"));
+const ChangePassword = lazy(() => import("./components/ChangePassword"));
 
 function App() {
   return (
@@ -18,17 +20,26 @@ function App() {
       <CartProvider>
         <Navigation />
         <Container className="py-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Products />} />
-            <Route path="/product/:id" element={<ProductDetail />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/change-password" element={<ChangePassword />} />{" "}
-            {/* ✅ Route add */}
-          </Routes>
+          {/* ✅ Suspense - Loader dikhayega jab tak component load ho */}
+          <Suspense
+            fallback={
+              <div className="text-center py-5">
+                <Spinner animation="border" variant="primary" />
+                <p className="mt-2">Loading...</p>
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/store" element={<Products />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/change-password" element={<ChangePassword />} />
+            </Routes>
+          </Suspense>
         </Container>
       </CartProvider>
     </BrowserRouter>
